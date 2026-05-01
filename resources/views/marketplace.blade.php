ab055f1041227dd51f8a963c6782661771c377e1
IMAM SODIK <imamsodik00@gmail.com>
2026-05-01 21:08:17 +0700

    asdf

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -199,9 +199,8 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}">
+    <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="{{ config('midtrans.client_key') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </script>
     <script>
         function showPaymentPopup(type) {
 
